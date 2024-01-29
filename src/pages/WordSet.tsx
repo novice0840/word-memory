5dcb2c1008af9d359acf2305858270e474af9016
novice0840 <magnolia0840@gmail.com>
2024-01-29 15:45:45 +0900

    feat: 1.0

--- src/pages/WordSet.tsx
+++ src/pages/WordSet.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState, MouseEvent } from "react";
 import JLPTword from "../words/JLPTwords.json";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import { Container, Button, Typography, Stack } from "@mui/material";
 
 type TotalWords = {
@@ -19,11 +19,12 @@ const WordSetPage = () => {
   const { [rate]: words } = totalWords;
   const totalIndex = words.length;
   const [curIndex, setCurIndex] = useState<number>(0);
-  const [koreanHidden, setKoreanHidden] = useState<boolean>(false);
-  const [hiraganaHidden, setHiraganaHidden] = useState<boolean>(false);
+  const [koreanHidden, setKoreanHidden] = useState<boolean>(true);
+  const [hiraganaHidden, setHiraganaHidden] = useState<boolean>(true);
+  const navigate = useNavigate();
+  const memoryList = JSON.parse(localStorage.getItem(rate) as string);
 
   useEffect(() => {
-    const memoryList = JSON.parse(localStorage.getItem(rate) as string);
     let nextIndex = curIndex;
     while (memoryList.includes(nextIndex)) {
       nextIndex += 1;
@@ -38,7 +39,11 @@ const WordSetPage = () => {
     } else if (buttonId === "hiragana") {
       setHiraganaHidden(!hiraganaHidden);
     } else if (buttonId === "memorization") {
-      const memoryList = JSON.parse(localStorage.getItem(rate) as string);
+      if (memoryList.length == totalIndex - 1) {
+        navigate("/");
+        localStorage.setItem(rate, JSON.stringify([]));
+        return 0;
+      }
       let nextIndex = curIndex + 1;
       while (memoryList.includes(nextIndex)) {
         nextIndex += 1;
@@ -46,23 +51,38 @@ const WordSetPage = () => {
       memoryList?.push(curIndex);
       localStorage.setItem(rate, JSON.stringify(memoryList));
       setCurIndex(nextIndex);
+      setKoreanHidden(true);
+      setHiraganaHidden(true);
     } else if (buttonId === "again") {
-      const memoryList = JSON.parse(localStorage.getItem(rate) as string);
       let nextIndex = curIndex + 1;
       while (memoryList.includes(nextIndex)) {
         nextIndex += 1;
       }
+      setKoreanHidden(true);
+      setHiraganaHidden(true);
       setCurIndex(nextIndex);
     }
   };
 
   return (
     <Container sx={{ height: "100vh" }}>
-      <Stack direction="column" justifyContent="center" alignItems="center" spacing={2}>
+      <Stack
+        sx={{ height: "100vh" }}
+        direction="column"
+        justifyContent="center"
+        alignItems="center"
+        spacing={2}
+      >
+        <Button variant="contained" onClick={() => navigate("/")}>
+          메인 메뉴
+        </Button>
         <Typography>
-          {curIndex}/{totalIndex}
+          전체 단어 {curIndex}/{totalIndex}
         </Typography>
-        <Typography>{words[curIndex].pronunciation}</Typography>
+        <Typography>
+          외운 단어 {memoryList.length}/{totalIndex}
+        </Typography>
+        <Typography variant="h3">{words[curIndex].pronunciation}</Typography>
         <Typography> {hiraganaHidden ? "히라가나 숨김" : words[curIndex].original}</Typography>
         <Typography>{koreanHidden ? "한국어 숨김" : words[curIndex].koreans}</Typography>
         <Stack direction="row" spacing={2}>
