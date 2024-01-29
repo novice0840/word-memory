42416d174643d6e7d9afd63c06125cb4a3576179
novice0840 <magnolia0840@gmail.com>
2024-01-29 16:04:33 +0900

    feat: localStoreage 기본값 추가

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,8 +1,27 @@
 import { Link } from "react-router-dom";
 import { Button, Stack, Container } from "@mui/material";
 import TagFacesIcon from "@mui/icons-material/TagFaces";
+import { useEffect } from "react";
 
 const MainPage = () => {
+  useEffect(() => {
+    if (!localStorage.getItem("N1")) {
+      localStorage.setItem("N1", JSON.stringify([]));
+    }
+    if (!localStorage.getItem("N2")) {
+      localStorage.setItem("N2", JSON.stringify([]));
+    }
+    if (!localStorage.getItem("N3")) {
+      localStorage.setItem("N3", JSON.stringify([]));
+    }
+    if (!localStorage.getItem("N4")) {
+      localStorage.setItem("N4", JSON.stringify([]));
+    }
+    if (!localStorage.getItem("N5")) {
+      localStorage.setItem("N5", JSON.stringify([]));
+    }
+  }, []);
+
   return (
     <Container sx={{ marginTop: "150px" }}>
       <Stack spacing={5} alignItems="center" justifyContent="center">
