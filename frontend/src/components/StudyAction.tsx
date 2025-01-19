6797092dc6543fb86ab3ad0b44a758491c846c39
novice0840 <magnolia0840@gmail.com>
2025-01-19 21:52:04 +0900

    refactor: StudyAction 컴포넌트 반응형 추가

--- frontend/src/components/StudyAction.tsx
+++ frontend/src/components/StudyAction.tsx
@@ -7,19 +7,21 @@ interface StudyActionProps {
 
 const StudyAction = ({ onClick }: StudyActionProps) => {
   return (
-    <div className="fixed bottom-0 left-0 w-full p-4 grid grid-cols-2 grid-rows-2 gap-4 ">
-      <Button id="showMeaning" onClick={onClick}>
-        뜻 보기
-      </Button>
-      <Button id="showExampleSentences" onClick={onClick}>
-        예문 보기
-      </Button>
-      <Button id="memorization" onClick={onClick}>
-        암기 완료
-      </Button>
-      <Button id="again" onClick={onClick}>
-        다시 외우기
-      </Button>
+    <div className="fixed bottom-0 left-0  w-full p-4">
+      <div className="grid grid-cols-2 grid-rows-2 gap-4 max-w-md mx-auto">
+        <Button id="showMeaning" onClick={onClick}>
+          뜻 보기
+        </Button>
+        <Button id="showExampleSentences" onClick={onClick}>
+          예문 해석 보기
+        </Button>
+        <Button id="memorization" onClick={onClick}>
+          암기 완료
+        </Button>
+        <Button id="again" onClick={onClick}>
+          다시 외우기
+        </Button>
+      </div>
     </div>
   );
 };
