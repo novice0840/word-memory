3b7c5d7d35110b0cb582b551f28ad625bd678373
novice0840 <magnolia0840@gmail.com>
2025-01-22 17:57:42 +0900

    fix: 타입에러 해결

--- frontend/src/utils/word.ts
+++ frontend/src/utils/word.ts
@@ -1,4 +1,4 @@
-import { Level } from "@/types/word";
+import { Level, Word } from "@/types/word";
 import {
   JLPT_N1_WORDS,
   JLPT_N2_WORDS,
@@ -22,13 +22,13 @@ export const getNextIndex = (
   return nextIndex;
 };
 
-export const getJLPTWords = (level: Level) => {
+export const getJLPTWords = (level: Level): Word[] => {
   const levelWords = {
     N1: JLPT_N1_WORDS,
     N2: JLPT_N2_WORDS,
     N3: JLPT_N3_WORDS,
     N4: JLPT_N4_WORDS,
     N5: JLPT_N5_WORDS,
-  };
+  } as Record<Level, Word[]>;
   return levelWords[level];
 };
