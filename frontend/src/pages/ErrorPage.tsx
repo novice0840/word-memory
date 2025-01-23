7d30cfbdb158385b544a3be287060736869870ba
novice0840 <magnolia0840@gmail.com>
2025-01-23 11:38:23 +0900

    feat: 에러 페이지 UI 변경

--- frontend/src/pages/ErrorPage.tsx
+++ frontend/src/pages/ErrorPage.tsx
@@ -1,12 +1,15 @@
 import { Button } from "@/components/ui/button";
 import { Link } from "react-router-dom";
+import { ERROR_PAGE_TITLE, ERROR_PAGE_BUTTON_TEXT } from "@/constants/message";
+import { DangerCircle } from "@mynaui/icons-react";
 
 const ErrorPage = () => {
   return (
     <div className="flex flex-col items-center justify-center h-screen gap-4">
-      <h1 className="text-4xl font-extrabold">잘못된 페이지입니다.</h1>
-      <Button asChild className="text-xl">
-        <Link to={`/`}>메인 페이지로 이동</Link>
+      <DangerCircle className="w-16 h-16 text-red-500" />
+      <h1 className="text-xl font-extrabold">{ERROR_PAGE_TITLE}</h1>
+      <Button asChild>
+        <Link to="/">{ERROR_PAGE_BUTTON_TEXT}</Link>
       </Button>
     </div>
   );
