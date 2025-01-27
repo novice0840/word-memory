c0ea405fe33f63632a0f7a6f5075fb0c64fe0f88
novice0840 <magnolia0840@gmail.com>
2025-01-27 23:22:40 +0900

    test: 테스트 코드 추가

--- frontend/src/tests/pages/WordsPage.test.tsx
+++ frontend/src/tests/pages/WordsPage.test.tsx
@@ -1,11 +1,34 @@
 import { routes } from "@/App";
 import { createMemoryRouter, RouterProvider } from "react-router-dom";
-import { describe, it, expect, beforeAll } from "vitest";
-import { render, screen } from "@testing-library/react";
+import { describe, it, expect, vi, Mock, beforeEach } from "vitest";
+import { render, screen, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
+import { EXAMPLE_JLPT_WORDS } from "@/tests/constants";
+
+vi.mock("@/utils/word", () => ({
+  getJLPTWords: () => EXAMPLE_JLPT_WORDS,
+  isValidLevel: () => true,
+}));
 
 describe("WordsPage Test", () => {
-  beforeAll(() => {
+  it("홈 아이콘 클릭 시 메인페이지로 이동", async () => {
+    // Given
+    const router = createMemoryRouter(routes, {
+      initialEntries: ["/words/N1"],
+    });
+    render(<RouterProvider router={router} />);
+    const homeIconButton = screen.getByRole("link", { name: "homeIcon" });
+
+    // When
+    await userEvent.click(homeIconButton);
+
+    // Then
+    expect(router.state.location.pathname).toBe("/");
+  });
+});
+
+describe("WordsPage Test", () => {
+  beforeEach(() => {
     const router = createMemoryRouter(routes, {
       initialEntries: ["/words/N1"],
     });
@@ -13,21 +36,6 @@ describe("WordsPage Test", () => {
   });
 
   describe("Header Icons Test", () => {
-    it("홈 아이콘 클릭 시 메인페이지로 이동", async () => {
-      // Given
-      const router = createMemoryRouter(routes, {
-        initialEntries: ["/words/N1"],
-      });
-      render(<RouterProvider router={router} />);
-      const homeIconButton = screen.getByRole("link", { name: "homeIcon" });
-
-      // When
-      await userEvent.click(homeIconButton);
-
-      // Then
-      expect(router.state.location.pathname).toBe("/");
-    });
-
     it("설정 아이콘 클릭 시 설정 UI 표시", async () => {
       // Given
       const cogIconButton = screen.getByRole("button", { name: "cogIcon" });
@@ -58,31 +66,11 @@ describe("WordsPage Test", () => {
       // Given
       const menuIconButton = screen.getByRole("button", { name: "menuIcon" });
       const wordList = screen.getByLabelText("wordList");
-
       // When
       await userEvent.click(menuIconButton);
 
+      // Then
       expect(wordList).toHaveClass("translate-x-0");
-    });
-
-    it("WordList 컴포넌트에 있는 화살표 버튼을 누르면 WordList 컴포넌트는 사라지고 원래 페이지로 UI가 돌아온다", async () => {
-      // Given
-      const menuIconButton = screen.getByRole("button", { name: "menuIcon" });
-      const wordList = screen.getByLabelText("wordList");
-      const arrowLeftIconButton = screen.getByRole("button", {
-        name: "arrowLeftIcon",
-      });
-
-      // When
-      await userEvent.click(menuIconButton);
-
-      expect(wordList).toHaveClass("translate-x-0");
-      await userEvent.click(arrowLeftIconButton);
-      expect(wordList).toHaveClass("-translate-x-full");
-    });
-
-    it("WordList 컴포넌트에서 단어 클릭 시 단어 상세 페이지로 이동", () => {
-      expect(1).toBe(1);
     });
   });
 
