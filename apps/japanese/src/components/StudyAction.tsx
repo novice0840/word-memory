2fd29347d8066c72cdcfb033ca9d0562277c8771
novice0840 <magnolia0840@gmail.com>
2025-07-07 22:03:37 +0900

    refactor: 단어 뜻 보기 기능 URL params와 연동

--- apps/japanese/src/components/StudyAction.tsx
+++ apps/japanese/src/components/StudyAction.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Button } from "shared/ui";
 import { getWords } from "shared/utils";
-import { useParams } from "react-router-dom";
+import { useParams, useSearchParams } from "react-router-dom";
 import { useGetMemoryList } from "shared/hooks";
 
 interface StudyActionProps {
@@ -13,15 +13,31 @@ const StudyAction = ({ onClick }: StudyActionProps) => {
   const words = getWords(level, "japanese");
   const totalLength = words.length;
   const { memoryList } = useGetMemoryList(level);
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const isWordMeaningVisible =
+    searchParams.get("isWordMeaningVisible") === "true";
 
   const isAllWordsMemorized = [...Array(totalLength).keys()].every((i) =>
     memoryList.includes(i)
   );
 
+  const handleClickMeaning = () => {
+    const newParams = new URLSearchParams(searchParams);
+
+    if (isWordMeaningVisible) {
+      newParams.set("isWordMeaningVisible", "false");
+    } else {
+      newParams.set("isWordMeaningVisible", "true");
+    }
+
+    setSearchParams(newParams);
+  };
+
   return (
     <div className="w-full h-32 grid grid-cols-2 grid-rows-2 gap-12">
-      <Button id="meaning" onClick={onClick} className="h-full">
-        뜻 보기
+      <Button id="meaning" onClick={handleClickMeaning} className="h-full">
+        {isWordMeaningVisible ? "뜻 숨기기" : "뜻 보기"}
       </Button>
       <Button id="sentence" onClick={onClick} className="h-full">
         예문 해석 보기
--- apps/japanese/src/components/Word.tsx
+++ apps/japanese/src/components/Word.tsx
@@ -1,3 +1,5 @@
+import { useSearchParams } from "react-router-dom";
+
 interface WordProps {
   original: string | null;
   pronunciation: string;
@@ -6,12 +8,16 @@ interface WordProps {
 }
 
 const Word = ({ original, pronunciation, koreans, showMeaning }: WordProps) => {
+  const [searchParams] = useSearchParams();
+  const isWordMeaningVisible =
+    searchParams.get("isWordMeaningVisible") === "true";
+
   return (
     <div className="text-center h-32">
       <div className="text-4xl font-japanese mb-2">
         {original?.split("·").join(", ") || pronunciation}
       </div>
-      {showMeaning && (
+      {isWordMeaningVisible && (
         <div>
           <div>{original && pronunciation}</div>
           <div>{koreans?.join(", ")}</div>
