67b78651cd1ce9f1adff7c26ea07515c0f3fcb2b
novice0840 <magnolia0840@gmail.com>
2025-12-14 19:25:51 +0900

    fix: eslint no-unused-vars error -> warn로 변경

--- apps/japanese/.eslintrc.cjs
+++ apps/japanese/.eslintrc.cjs
@@ -12,5 +12,6 @@ module.exports = {
   rules: {
     "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
     "@typescript-eslint/no-explicit-any": "warn",
+    "@typescript-eslint/no-unused-vars": "warn",
   },
 };
